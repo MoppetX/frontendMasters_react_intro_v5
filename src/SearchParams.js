539587254fb94979b9b2001dab0aa5d30242cbdb
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-23 15:24:31 +0200

    finished the section on asynchronous API Requests; added a preferred browsers list to package.json

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,41 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 // parcel will install this automatically if not found in your dependencies :)
-import pet, { ANIMALS } from "@frontendmasters/pet";
-import useDropdown from "./useDropdown";
+import pet, { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 // console.log(ANIMALS);
 
 const SearchParams = () => {
-  const [location, updateLocation] = useState("Seattle, WA");
-  const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [location, updateLocation] = useState( 'Seattle, WA' );
+  const [breeds, setBreeds] = useState( [] );
+  const [animal, AnimalDropdown] = useDropdown( 'Animal', 'dog', ANIMALS );
+  const [breed, BreedDropdown, setBreed] = useDropdown( 'Breed', '', breeds );
+  const [pets, setPets] = useState( [] );
   
-  useEffect(() => {
-    setBreeds([]);
-    setBreed("");
+  async function requestPets() {
+    const { animals } = await pet.animals(
+      {
+        location,
+        breed,
+        type: animal
+      } );
     
-    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
-      const breedStrings = apiBreeds.map(({ name }) => name);
-      setBreeds(breedStrings);
-    }, console.error);
-  }, [animal, setBreed, setBreeds]);
+    // if nothing comes back from the API, setPets to an empty array
+    setPets(animals || [])
+  }
+  
+  useEffect( () => {
+    setBreeds( [] );
+    setBreed( '' );
+    
+    pet.breeds( animal ).then( ( { breeds: apiBreeds } ) => {
+      const breedStrings = apiBreeds.map( ( { name } ) => name );
+      setBreeds( breedStrings );
+    }, console.error );
+  }, [animal, setBreed, setBreeds] );
   // the above are the dependencies for useEffect - the variables that will cause useEffect to update should a
   // change occur to them
   
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={() => {
+        // this will prevent it from setting up an html post form
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input
             id="location"
             value={location}
             placeholder="Location"
-            onChange={e => updateLocation(e.target.value)}
+            onChange={e => updateLocation( e.target.value )}
           />
         </label>
-        <AnimalDropdown />
-        <BreedDropdown />
+        <AnimalDropdown/>
+        <BreedDropdown/>
         <button>Submit</button>
       </form>
     </div>
